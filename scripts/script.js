081cf9b1ad6862ab86498dd52d3afabe5dafa730
sovaos42 <sovaos42@gmail.com>
2023-02-02 13:37:35 +0700

    3

--- scripts/script.js
+++ scripts/script.js
@@ -2,23 +2,26 @@ Vue.component('notes', {
 
     template:
     `
-    <div class="all-notes">
-    <div class="row">
-        <div class="note">
-            <cards></cards>
+    <div class="all">
+        <div class="all-notes">
+        <div class="row">
+            <div class="note">
+                <cards></cards>
+            </div>
+            <div class="note">
+                <cards></cards>
+            </div>
+            <div class="note">
+                <cards></cards>
+            </div>
         </div>
-        <div class="note">
-            <cards></cards>
+            <div class="from-cards">
+                <form-cards></form-cards>
+            </div>
         </div>
-        <div class="note">
-            <cards></cards>
-        </div>
+        
     </div>
-        <div class="from-cards">
-            <form-cards></form-cards>
-        </div>
-    </div>
-    `
+    `,
 
 })
 
@@ -26,16 +29,36 @@ Vue.component('cards',{
 
     template:
     `
-    
     <div class="card">
     <h3>Card</h3>
-        <ul>
-            <li>drfbeegeer</li>
-            <li>ewgw</li>
-            <li>ewgweg</li>
-        </ul>
+    
+       <div>
+       
+                <ul>
+                      <li v-for="form in forms">
+                          <p>{{ form.name }}</p>
+                          <p>{{ form.list }}</p>
+                      </li>
+                </ul>
+        </div>
+        <notesForm @notes-form="addForm"></notesForm>
     </div>
-    `
+    `,
+    data(){
+        return{
+
+            forms: []
+
+        }
+    },
+    computed: {
+    },
+    methods: {
+        addForm(notesForm) {
+            this.forms.push(notesForm)
+        }
+
+    }
 })
 
 Vue.component('form-cards', {
@@ -43,17 +66,39 @@ Vue.component('form-cards', {
     template:
     `
     <div class="all-form-cards">
-        <form> 
-            <input type="text" placeholder="name">
-            <label for="list">Enter notes</label>
-            <textarea id="list" placeholder="Notes"></textarea>
-            <button type="submit">Add notes</button>
+    <div class="center">
+        <form @submit.prevent="onForm"> 
+            <input v-model="name" type="text" placeholder="name" class="input-name" required ><br>
+            <label for="list">Enter notes</label><br>
+            <textarea v-model="list" id="list" placeholder="Notes" required ></textarea><br>
+            <button type="submit" class="button-submit">Add notes</button>
         </form>
+        </div>
     </div>
-    `
+    `,
+    data(){
+        return{
+            name: null,
+            list: null,
+        }
+    },
+    methods:{
+        onForm(){
+            let notesForm = {
+                name: this.name,
+                list: this.list,
+            }
+            this.$emit('notes-form', notesForm)
+            this.name = null
+            this.list = null
+
+        }
+    }
 })
 
 let app = new Vue({
     el: '#app',
+    data:{
 
+    }
 })
