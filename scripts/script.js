fa997487f3e5043ef4421ff39acc2b063eed9b0e
sovaos42 <sovaos42@gmail.com>
2023-02-08 01:54:43 +0700

    перемещение по столбикам и дата

--- scripts/script.js
+++ scripts/script.js
@@ -21,13 +21,13 @@ Vue.component('notes', {
         <div class="all-notes">
         <div class="row" >
             <div class="note1">
-                <cards v-for="formCards in notes1" :formCards="formCards"></cards>
+                <cards :cardList="notes1" :AlertList="AlertList"></cards>
             </div>
             <div class="note2">
-                <cards v-for="formCards in notes2" :formCards="formCards"></cards>
+                <cards :cardList="notes2" :AlertList="AlertList"></cards>
             </div>
             <div class="note3">
-                <cards v-for="formCards in notes3" :formCards="formCards"></cards>
+                <cards :cardList="notes3" :AlertList="AlertList"></cards>
             </div>
         </div>
             <div class="from-cards">
@@ -47,8 +47,51 @@ Vue.component('notes', {
             }
         })
     },
-    methods:{
-    
+    methods: {
+        AlertList(cards, note) {
+
+            let count = 0; 
+            let num = 0; 
+
+            for (let i in cards.arrList) {
+                if (cards.arrList[i].title != null) 
+                    count++;
+
+                if (cards.arrList[i].title == note) 
+                    num = i;
+            }
+            
+            if (this.notes1.indexOf(cards) >= 0) { 
+                if (this.notes2.length < 5) { 
+                    if ((100 / count) * cards.count_t > 50) {
+                        this.notes2.push(cards);
+                        this.notes1.splice(this.notes1.indexOf(cards), 1)
+
+                        if (this.check == false && this.notes1.length != 3) 
+                            this.check = true;
+                    }
+                } else {
+                    cards.arrList[num].complete = false;
+                    cards.count_t -= 1;
+                    return;
+                }
+            }
+
+            if (this.notes2.indexOf(cards) >= 0) { 
+                if ((100 / count) * cards.count_t == 100) {
+                    cards.date = new Date().toLocaleString();
+                    this.notes3.push(cards);
+                    this.notes2.splice(this.notes2.indexOf(cards), 1);
+                }
+
+                return;
+            }
+
+            if (this.notes3.indexOf(cards) >= 0) {
+                cards.count_t -= 1;
+                return;
+            }
+        }
     }
 
 })
@@ -57,25 +100,44 @@ Vue.component('cards',{
     props: {
         formCards: {
             type: Object
+        },
+        cardList:[],
+        AlertList:{
+            type: Function
         }
     },
 
     template:
         `
         <div class="card">
-            <div class="conc">
-                    <p>{{ formCards.name }}</p>
+            <div class="conc" v-for="formCards in cardList">
+                    <p>{{ formCards.names }}</p>
                     <ul>
-                        <li v-for="list in formCards.arrList">
-                                {{list.title}}
-                            </div>
-                            
+                        <li class="notess" v-for="list in formCards.arrList">
+                        <div
+                            @click="formCards.count_t = check(list.complete, formCards.count_t),
+                            list.complete = true, AlertList(formCards, list.title)">
+                            {{list.title}}
+                        </div>
+                        <div v-if="list.title != null && list.complete === false">-</div >
+                        <div v-else-if="list.complete == true">+</div>
                         </li>
                     </ul>
+                    <div v-if="formCards.date != null">
+                        {{formCards.date}}
+                    </div>
             </div>
-        </div>
-        
-    `
+        </div>  
+    `,
+    methods:{
+        check(compl, count_t){
+            if(compl == false){
+                count_t++;
+                return count_t;
+            }
+            return count_t;
+        }
+    }
 })
 
 Vue.component('form-cards', {
@@ -92,7 +154,7 @@ Vue.component('form-cards', {
     <div class="center">
    <form @submit.prevent="onForm"> 
             <label for="name">Имя заметки</label>
-            <input v-model="name" id="name" type="text" placeholder="name" class="input-name"><br>
+            <input v-model="names" id="name" type="text" placeholder="name" class="input-name"><br>
             <input v-model="listOne" type="text" placeholder="Заметка 1">
             <input v-model="listTwo" type="text" placeholder="Заметка 2">
             <input v-model="listThree" type="text" placeholder="Заметка 3">
@@ -112,7 +174,7 @@ Vue.component('form-cards', {
     `,
     data(){
         return{
-            name: null,
+            names: null,
             listOne: null,
             listTwo: null,
             listThree: null,
@@ -125,18 +187,20 @@ Vue.component('form-cards', {
         onForm(){
             if(this.listOne && this.listTwo && this.listThree){
                 let formCards = {
-                    name: this.name,
+                    names: this.names,
                     arrList:[
                         {title: this.listOne, complete: false},
                         {title: this.listTwo, complete: false},
                         {title: this.listThree, complete: false},
                         {title: this.listFour, complete: false},
                         {title: this.listFive, complete: false},
-                    ]
+                    ],
+                    count_t: 0,
+                    date: null
                 }
                 eventBus.$emit('notes-form', formCards)
 
-                this.name = null
+                this.names = null
                 this.listOne = null
                 this.listTwo = null
                 this.listThree = null
@@ -145,7 +209,7 @@ Vue.component('form-cards', {
             }
             else{
                 
-                if(!this.name) this.errors.push("Введите заголовок.")
+                if(!this.names) this.errors.push("Введите заголовок.")
                 if(!this.listOne) this.errors.push("Заполните заметку 1.")
                 if(!this.listTwo) this.errors.push("Заполните заметку 2.")
                 if(!this.listThree) this.errors.push("Заполните заметку 3.")
